23883cb53877790bc7c438ee10230c2a43bc0b93
unknown <gnitish31@gmail.com>
2021-02-06 16:58:26 +0530

    logout all feature added

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
         const token = req.cookies.jwt
         const verifyUser = jwt.verify(token, process.env.SECRET)
         console.log(verifyUser)
-        const user = await Register.findOne({_id:verifyUser._id})
+        const user = await Register.findOne({"tokens.token":token})
         console.log(user)
         req.token=token;
         req.user=user;
